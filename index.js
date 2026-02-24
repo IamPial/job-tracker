cf9da4f837e354b6e578e696742691ff0485a3ca
IamPial <pialname394@gmail.com>
2026-02-24 07:36:30 +0600

    render filtered interview to interview section

--- index.js
+++ index.js
@@ -68,6 +68,7 @@ const rejectCount = document.getElementById("display-reject");
 
 const allSectionCards = document.getElementById("allCards");
 const mainContainer = document.querySelector("main");
+const filterCards = document.getElementById("filter-cards");
 
 // for toggling all necessary buttons
 const allFilterBtn = document.getElementById("allFilterBtn");
@@ -105,30 +106,91 @@ function toggleChange(id) {
 // Executing Main Content
 
 mainContainer.addEventListener("click", function (e) {
-  const parentNode = e.target.parentNode.parentNode;
+  if (e.target.classList.contains("interview-btn")) {
+    const parentNode = e.target.parentNode.parentNode;
+    const companyName = parentNode.querySelector(".companyName").innerText;
+    const position = parentNode.querySelector(".position").innerText;
+    const location = parentNode.querySelector(".location").innerText;
+    const type = parentNode.querySelector(".type").innerText;
+    const salary = parentNode.querySelector(".salary").innerText;
+    const description = parentNode.querySelector(".description").innerText;
+    const status = parentNode.querySelector(".my-status").innerText;
 
-  const companyName = parentNode.querySelector(".companyName").innerText;
-  const position = parentNode.querySelector(".position").innerText;
-  const location = parentNode.querySelector(".location").innerText;
-  const type = parentNode.querySelector(".type").innerText;
-  const salary = parentNode.querySelector(".salary").innerText;
-  const description = parentNode.querySelector(".description").innerText;
-  const status = parentNode.querySelector(".my-status").innerText;
+    const jobInfo = {
+      companyName,
+      position,
+      location,
+      type,
+      salary,
+      description,
+      status,
+    };
 
-  const jobInfo = {
-    companyName,
-    position,
-    location,
-    type,
-    salary,
-    description,
-    status,
-  };
-
-  const filterInterview = interviewList.find(
-    (item) => item.companyName == jobInfo.companyName,
-  );
-  if (!filterInterview) {
-    interviewList.push(jobInfo);
+    parentNode.querySelector(".my-status").innerText =
+      "interview".toUpperCase();
+    parentNode.querySelector(".my-status").className =
+      "badge badge-outline badge-success bg-success/10";
+    const filterInterview = interviewList.find(
+      (item) => item.companyName == jobInfo.companyName,
+    );
+    if (!filterInterview) {
+      interviewList.push(jobInfo);
+    }
+    interviewSection();
   }
 });
+
+//for render of interview list
+function interviewSection() {
+  filterCards.innerHTML = "";
+  for (const interview of interviewList) {
+    let div = document.createElement("div");
+    div.className = "card bg-base-300 max-w-full flex flex-row justify-between";
+    div.innerHTML = `
+        <div class="card-body">
+            <h2 class="companyName card-title text-gray-800 text-2xl font-bold">
+              DataViz Solutions 3
+            </h2>
+            <h4 class="position text-[#64748B] text-[16px]">
+              Data Visualization Specialist
+            </h4>
+            <div class="text-[#64748B] text-[16px] space-x-3">
+              <span class="location">Boston, MA</span>
+              <span class="type">Full-time</span>
+              <span class="salary">125,000 - $165,000</span>
+            </div>
+            <p
+              class="my-status badge badge-outline badge-primary bg-primary/10"
+            >
+              NOT APPLIED
+            </p>
+            <p class="description text-[#323B49] text-[16px]">
+              Transform complex data into compelling visualizations. Required
+              skills: D3.js, React, and strong analytical thinking.
+            </p>
+            <div class="card-actions">
+              <button
+                id="interview-btn"
+                class="interview-btn btn border-success bg-success/50"
+              >
+                INTERVIEW
+              </button>
+              <button
+                id="reject-btn"
+                class="reject-btn btn border-error bg-error/50"
+              >
+                REJECT
+              </button>
+            </div>
+          </div>
+          <div class="delete-icon mt-5 p-5">
+            <button
+              class="btn h-10 w-10 btn-error bg-error/50 transition-all duration-200 active:scale-90 rounded-full"
+            >
+              <i class="fa-regular fa-trash-can"></i>
+            </button>
+          </div>
+    `;
+    filterCards.appendChild(div);
+  }
+}
