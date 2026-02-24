ed67e6c6108be63dbb0aaf7680e3a0ff3e869145
IamPial <pialname394@gmail.com>
2026-02-24 15:51:45 +0600

    and remove the rejectlist section when it is empty And filled it out with empty-card section

--- index.js
+++ index.js
@@ -173,7 +173,6 @@ function toggleChange(id) {
     } else {
       emptyCardsSection.classList.add("hidden");
     }
-
     interviewSection();
   }
 
@@ -235,6 +234,12 @@ mainContainer.addEventListener("click", function (e) {
     if (currentStatus == "rejectFilterBtn") {
       availAbleJobsCount.innerText = `${rejectList.length} of ${allCards.length}`;
 
+      // added the empty-card-section for rejection section
+      if (rejectList.length == "") {
+        emptyCardsSection.classList.remove("hidden");
+      } else {
+        emptyCardsSection.classList.add("hidden");
+      }
       rejectSection();
     }
 
